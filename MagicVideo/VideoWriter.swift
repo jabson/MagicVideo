38de1bb11f820477d663b7e4fe0d31be7a342e2e
Jaba Odishelashvili <jodishelashvili@bog.ge>
2022-11-21 14:36:21 +0400

    Added empty line

--- MagicVideo/VideoWriter.swift
+++ MagicVideo/VideoWriter.swift
@@ -14,6 +14,7 @@ class VideoWriter: NSObject {
     var videoInput: AVAssetWriterInput
     var audioInput: AVAssetWriterInput
     
+    
     private var isFirstVideoFrame = true
     
     init(fileUrl: URL, size: CGSize) {
